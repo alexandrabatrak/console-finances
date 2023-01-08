60dd78a7a6cc97b6c5e577974917ee963b23f772
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-08 18:26:41 +0000

    Clean up comments, more tests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /**
  *  Console Finances
+ *  @author Alexandra Batrak
+ *
  *  To find:
  *    - Total months
  *    - Total net sum
@@ -17,6 +19,9 @@
  *  The best method will depend on specific case and size of the array - in this application array is small,
  *  so performance wouldn't make much difference, but for larger arrays 'for' or 'for...of' loops seem to be a
  *  better solution rather than using a high-order function whose perfomance slows down as the size of the array increases.
+ *
+ *  (p.s. Comments format for 'Better Comments' extension styling. Due to a lot of commenting for code, all text comments are marked // //
+ *  Headings and subheading are * and **.)
  */
 var finances = [
   ['Jan-2010', 867884],
@@ -116,13 +121,13 @@ console.log(`Total Months: ${totalMonths}`);
 // *The net total amount of Profit/Losses over the entire period.*
 let totalNetSum = 0;
 // **METHOD 1: FOR LOOP*
-// example: better performance, more code
+// //example: better performance, more code
 // function calculateSum(finances) {
 //   for (let i = 0; i < totalMonths; i++) {
 //     if (typeof finances[i] === 'number') {
 //       totalNetSum += finances[i];
 
-//       // recursive function to iterate from nested elements too
+// // recursive function to iterate from nested elements too
 //     } else if (Array.isArray(finances[i])) {
 //       calculateSum(finances[i]);
 //     }
@@ -141,10 +146,10 @@ let totalNetSum = 0;
 // }
 // calculateSum(finances);
 
-// **METHOD 2: REDUCE
-// example: minimal code, better if performance not critical
+// **METHOD 2: REDUCE*
+// // example: minimal code, better if performance not critical
 totalNetSum = finances.reduce((accumulator, value) => {
-  // add the second element in the array
+  // // add the second element in the array
   return accumulator + value[1];
 }, 0);
 
@@ -152,19 +157,20 @@ console.log(`Total: £${totalNetSum}`);
 
 // *The average of the **changes** in Profit/Losses over the entire period.*
 let differences = [];
+
 // **METHOD 1: FOR LOOP*
-// start with i = 1 to start comparison from the second month
+// // start with i = 1 to start comparison from the second month
 for (let i = 1; i < totalMonths; i++) {
   let difference = finances[i][1] - finances[i - 1][1];
   differences.push(difference);
 }
 
 // **METHOD 2: SLICE AND MAP*
-// start from the second element in slice
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// // start from the second element in slice
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 // differences = finances
 //   .slice(1)
-//   //iterative map method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// //iterative map method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 //   .map((element, index) => element[1] - finances[index][1]);
 
 // *Calculate the sum of differences*
@@ -174,8 +180,7 @@ for (let i = 1; i < totalMonths; i++) {
 //   differenceSum += differences[i];
 // }
 // **METHOD 1.1: FOR...OF LOOP*
-// above as for...of loop
-// let differenceSum = 0;
+// differenceSum = 0;
 // for (let value of differences) {
 //   differenceSum += value;
 // }
@@ -183,17 +188,17 @@ for (let i = 1; i < totalMonths; i++) {
 // let averageChange = differenceSum / (totalMonths - 1);
 
 // **METHOD 2: REDUCE*
-// MERGE averageChange with REDUCE
+// // Get averageChange in one go with array.reduce
 let averageChange =
   differences.reduce((accumulator, value) => {
     return accumulator + value;
   }, 0) /
   (totalMonths - 1);
-// toFixed to round it up to the nearst 100th
+// // toFixed to round it up to the nearst 100th
 console.log(`Average Change: £${averageChange.toFixed(2)}`);
 
 // *The greatest increase and decrease in profits (date and amount) over the entire period.*
-// define variables (only because I'm using multiple methods and it's easier to test them this way, and also minimise redeclaring them for each method)
+// // define variables (because I'm using multiple methods and it's easier to test them this way, and also minimise redeclaring them for each method)
 let profitGain;
 let profitLoss;
 let profitGainMonth;
@@ -203,15 +208,14 @@ let profitLossMonth;
 // profitGain = differences.reduce((accumulator, value) => Math.max(accumulator, value));
 // profitLoss = differences.reduce((accumulator, value) => Math.min(accumulator, value));
 // **METHOD 2: MATH.MAX & INDEXOF*
-
 // profitGain = Math.max(...differences);
 // profitLoss = Math.min(...differences);
-// SAME FOR BOTH METHODS
+// // SAME FOR BOTH METHODS
 // // 2 arrays essentially have same position of elements, but differences is 1 less, as we skip January. So, adding +1 lets access the index of the nested array inside the finances that is corresponding to the max number in differences array
 // let profitGainMonth = finances[differences.indexOf(profitGain) + 1][0];
 // let profitLossMonth = finances[differences.indexOf(profitLoss) + 1][0];
 
-// Set variables for loops starting at the first element in differences array
+// // Initialise value for loops starting at the first element in differences array
 profitGain = differences[0];
 profitLoss = differences[0];
 // **METHOD 3: FOR LOOP*
@@ -241,37 +245,35 @@ for (let difference of differences) {
 console.log(`Greatest Gain in Profits: ${profitGainMonth} (£${profitGain})`);
 console.log(`Greatest Decrease in Profits ${profitLossMonth} (£${profitLoss})`);
 // ==========================================================
-// *EXTRAS: display data on the page
+// *EXTRAS: display data on the page*
 
-// get time period from calculating date difference of first element in array to the last
-// TODO: Loop through variables to apply toLocaleString, instead of doing it for each one separately
+// // get time period from calculating date difference of first element in array to the last
 let firstMonth = new Date(finances[0][0]);
 let lastMonth = new Date(finances[finances.length - 1][0]);
 const timeFrame = lastMonth.getTime() - firstMonth.getTime();
-// convert timeFrame from milliseconds to months
+// // convert timeFrame from milliseconds to months
 const timeFrameinMonths = timeFrame / (1000 * 60 * 60 * 24 * (365 / 12));
 const years = Math.floor(timeFrameinMonths / 12);
-// get the remainder from division
+// // get the remainder from division
 const months = Math.floor(timeFrameinMonths % 12);
 
 const totalMonthsHTML = document.getElementById('totalMonths');
-// it's so similar to php method, easy peasy lol
 totalMonthsHTML.innerHTML = `${years} year${
   years === 1 ? '' : 's'
 }, ${months} month${months === 1 ? '' : 's'}`;
 
-// get all elements
+// // get all elements
 const totalNetSumHTML = document.getElementById('totalNetSum');
 const averageChangeHTML = document.getElementById('averageChange');
 const profitGainMonthHTML = document.getElementById('profitGainMonth');
 const profitGainAmountHTML = document.getElementById('profitGainAmount');
 const profitLossMonthHTML = document.getElementById('profitLossMonth');
 const profitLossAmountHTML = document.getElementById('profitLossAmount');
-// apply month values
+// // apply month values
 profitGainMonthHTML.innerHTML = profitGainMonth;
 profitLossMonthHTML.innerHTML = profitLossMonth;
 
-// Loop to display number as currency with toLocaleString
+// // Loop to display number as currency with toLocaleString
 const html = [
   totalNetSumHTML,
   averageChangeHTML,
@@ -280,8 +282,8 @@ const html = [
 ];
 const values = [totalNetSum, averageChange, profitGain, profitLoss];
 
-// undefined for user's pre-set locale
-// + apply brackets only for increase/decrease
+// // undefined for user's pre-set locale
+// // + apply brackets only for increase/decrease
 for (let i = 0; i < html.length; i++) {
   html[i].innerHTML = ` ${
     values[i] === profitGain || values[i] === profitLoss ? '(' : ''
@@ -291,7 +293,7 @@ for (let i = 0; i < html.length; i++) {
   })}${values[i] === profitGain || values[i] === profitLoss ? ')' : ''}`;
 }
 
-// Substract header and footer height from main height to fit everything within 100vh.
+// // Substract header and footer height from main height to fit everything within 100vh.
 let main = document.querySelector('main');
 let header = document.querySelector('header').clientHeight;
 let footer = document.querySelector('footer').clientHeight;
